fee32092a5202c73d7e62c625a287f3b6a1ab4f6
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2021-05-01 21:38:12 +0200

    try to init lcd (debugging mode)

--- lcd2004_i2c/pcf8574_lcd.c
+++ lcd2004_i2c/pcf8574_lcd.c
@@ -28,6 +28,25 @@
  * function:
  * - connect LEDs with output of PCF8574
  * - let all LED blink
+ *
+ * !!! TODO:   !!!!
+ * - handle usleep EINTR
+ *
+ *
+ * !!!! REAL stuff !!!!
+ *
+ *  Pinning:
+ *
+ *  LCD    PCF8574
+ * ----------------
+ *   rs  --  P0
+ *   rw  --  P1
+ *   en  --  P2
+ *   bl  --  P3  (backlight)
+ *  db4  --  P4
+ *  db5  --  P5
+ *  db6  --  P6
+ *  db7  --  P7
  */
 
 #include <stdio.h>
@@ -39,6 +58,33 @@
 #include <sys/ioctl.h>
 #include <errno.h>
 #include <linux/i2c-dev.h>
+
+
+/* define bit/pin positions to be used below */
+enum bit_pos_priv {
+	BIT0 = 1 << 0,
+	BIT1 = 1 << 1,
+	BIT2 = 1 << 2,
+	BIT3 = 1 << 3,
+	BIT4 = 1 << 4,
+	BIT5 = 1 << 5,
+	BIT6 = 1 << 6,
+	BIT7 = 1 << 7
+};
+
+/*
+ * Define pins and set pin to high!
+ *
+ * PCF8547 <-> LCD
+ *  P7  P6  P5  P4 P3 P2 P1 P0
+ *  |   |   |   |  |  |  |  |
+ * DB7 DB6 DB5 DB4 BL EN RW RS
+ */
+#define RS BIT0
+#define RW BIT1
+#define EN BIT2
+#define BL BIT3
+
 
 /* the first PCF8574 address -> LCD */
 #define I2C_ADDR_LCD 0x22
@@ -84,9 +130,104 @@ void __attribute__((noreturn)) blink_leds(void)
 	}
 }
 
+
+/*
+ * toogle EN and keep BL on
+ */
+void toogle_en_pin(int fd)
+{
+	int value = EN | BL;
+
+	printf("erstes byte -> value 0x%x in %s\n", value, __FUNCTION__);
+
+	if (write(fd, &value, 1) != 1) {
+		printf("write error: %s\n", strerror(errno));
+	}
+
+	int err = usleep(1); /* PWen > 450ns */
+
+	value = BL;
+	printf("zweites byte -> value 0x%x in %s\n", value, __FUNCTION__);
+
+	if (write(fd, &value, 1) != 1) {
+		printf("write error: %s\n", strerror(errno));
+	}
+
+	err = usleep(1);
+}
+
+void write_cmd(int fd, unsigned char data)
+{
+	int value = (data & 0xF0) | BL;
+
+	printf("erstes byte -> value 0x%x in %s\n", value, __FUNCTION__);
+
+	if (write(fd, &value, 1) != 1) {
+		printf("write error: %s\n", strerror(errno));
+	}
+
+	toogle_en_pin(fd);
+
+	value = ((data << 4) & 0xF0) | BL;
+	printf("zweites byte -> value 0x%x in %s\n", value, __FUNCTION__);
+	if (write(fd, &value, 1) != 1) {
+		printf("write error: %s\n", strerror(errno));
+	}
+}
+
+/*
+ * Init LCD:
+ *
+ * - Power supply on -> HD44780 goes through reset, so the following
+ *   is not always needed, but doing it does not harm and brings the
+ *   controller up in defined state
+ * - RS R/W DB7 DB6 DB5 DB4
+ *   0   0   0   0   1   1   -> 0x03  (Function set -> 8 bit mode)
+ * - wait for > 4.1ms
+ * - RS R/W DB7 DB6 DB5 DB4
+ *   0   0   0   0   1   1   -> 0x03  (Function set -> 8 bit mode)
+ * - wait for > 100us
+ * - RS R/W DB7 DB6 DB5 DB4
+ *   0   0   0   0   1   1   -> 0x03  (Function set -> 8 bit mode)
+ *
+ * Setup the LCD to wanted operation mode:
+ * - RS R/W DB7 DB6 DB5 DB4
+ *   0   0   0   0   1   0   -> 0x02  (Function set -> 4 bit mode)
+ *
+ */
+void init_lcd(void)
+{
+	int value = LED_OFF;
+	int fd = -1;
+
+	fd = open("/dev/i2c-1", O_RDWR);
+	if (fd < 0) {
+		printf("open error: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if (ioctl(fd, I2C_SLAVE, I2C_ADDR_LED) < 0) {
+		printf("ioctl error: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	write_cmd(fd, 0x03);
+	usleep(BLINK_TIME);
+	write_cmd(fd, 0x03);
+	usleep(BLINK_TIME);
+	write_cmd(fd, 0x03);
+
+	usleep(BLINK_TIME);
+	write_cmd(fd, 0x02);
+
+
+	close(fd);
+}
+
+
 int main(void)
 {
-	blink_leds();
+	init_lcd();
 
 	return EXIT_SUCCESS;
 }
